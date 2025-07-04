15285cd6a44fa7d45e87a92d101641a10952c4f9
KEVAT PATEL <kevatpatel46@gmail.com>
2025-07-04 22:25:43 +0530

    5th test(1\n,2,3 pass case) commit

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,13 +1,12 @@
 package org.example;
 
 public class StringCalculator {
-
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
 
-        String[] parts = numbers.split(",");
+        String[] parts = numbers.split(",|\n");
         int sum = 0;
         for (String part : parts) {
             sum += Integer.parseInt(part);

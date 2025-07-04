2097f3bd2757947df06ad707711ed15d9651c077
KEVAT PATEL <kevatpatel46@gmail.com>
2025-07-04 22:36:56 +0530

    7th test(negativeno pass case) commit

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,16 +1,45 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
     public int add(String numbers) {
-        if (numbers.isEmpty()) return 0;
-
-        String[] parts = numbers.split(",|\n");
-        int sum = 0;
-        for (String part : parts) {
-            sum += Integer.parseInt(part);
+        if (numbers.isEmpty()) {
+            return 0;
         }
+
+        String delimiter = ",|\n"; // default
+
+        if (numbers.startsWith("//")) {
+            int newlineIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, newlineIndex);
+            numbers = numbers.substring(newlineIndex + 1);
+        }
+
+        String[] parts = numbers.split(delimiter);
+        int sum = 0;
+        List<Integer> negatives = new ArrayList<>();
+
+        for (String part : parts) {
+            int num = Integer.parseInt(part);
+            if (num < 0) {
+                negatives.add(num);
+            } else {
+                sum += num;
+            }
+        }
+
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "negative numbers not allowed " +
+                            negatives.toString().replace("[", "<").replace("]", ">")
+            );
+        }
+
         return sum;
     }
+
 
 
 

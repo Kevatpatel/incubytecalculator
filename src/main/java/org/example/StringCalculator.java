2e59735168713c63abffad9dd555784853b795fd
KEVAT PATEL <kevatpatel46@gmail.com>
2025-07-04 22:08:21 +0530

    3rd test(twonumbercomma-passcase) commit

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,8 +2,15 @@ package org.example;
 
 public class StringCalculator {
     public int add(String numbers) {
-        if (numbers.isEmpty()) return 0;
-        return Integer.parseInt(numbers);  // handles only one number
-    }
+        if (numbers.isEmpty()) {
+            return 0;
+        }
 
+        String[] parts = numbers.split(",");
+        int sum = 0;
+        for (String part : parts) {
+            sum += Integer.parseInt(part);
+        }
+        return sum;
+    }
 }

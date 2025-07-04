dee806a4d12b31e51be741fa913f60a11579a345
KEVAT PATEL <kevatpatel46@gmail.com>
2025-07-04 21:56:43 +0530

    2nd test(singlenumber-failcase) commit

--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -9,4 +9,11 @@ public class StringCalculatorTest extends TestCase {
         int result = calculator.add("");
         assertEquals(0, result);
     }
+
+    public void testShouldReturnSingleNumber() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1");
+        assertEquals(1, result);
+    }
+
 }
